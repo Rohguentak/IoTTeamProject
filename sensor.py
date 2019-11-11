e97700713e8d8f46a58a48a37a3cf59feb03df09
Rohguentak <rhgt78@gmail.com>
2019-11-11 06:53:02 +0000

    add analog read and pulse sensor

--- sensor.py
+++ sensor.py
@@ -1,6 +1,16 @@
 import sys
 import Adafruit_DHT
+import spidev
+import time
 
+spi = spidev.Spidev()
+spi.open(0,0)
+spi.max_speed_hz = 1350000
+
+def analog_read(channel):
+	r = spi.xfer2([1,(8+channel) <<4,0)
+	adc_out = ((r[1]&3) << 8) + r[2]
+	return adc_out
 def temp_sensor():
 	
 	sensor = 'Adafruit_DHT.DHT11' ## using dht11
@@ -12,4 +22,7 @@ def temp_sensor():
 		print('fail to get read data')
 		sys.exit(1)
 
+def pulse_sensor():
+	reading = analog_read(0)
+	
 
